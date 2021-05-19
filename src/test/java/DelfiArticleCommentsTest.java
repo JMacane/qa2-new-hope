4ff5788b9f808ed357f527d29a1e193a68caba0d
Jelena Macane <jmacane@inbox.lv>
2021-05-19 20:31:43 +0300

    Delfi test added Logging added

--- src/test/java/DelfiArticleCommentsTest.java
+++ src/test/java/DelfiArticleCommentsTest.java
@@ -1,3 +1,5 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -18,29 +20,39 @@ public class DelfiArticleCommentsTest {
     private final By HOME_PAGE_ARTICLE = By.tagName("article");
 
     private final By ARTICLE_PAGE_TITLE = By.xpath(".//h1[contains(@class, 'text-size-md-30')]");
+
     private final By ARTICLE_PAGE_COMMENTS = By.xpath(".//a[contains(@class, 'text-size-md-28')]");
+
+    private final Logger LOGGER = LogManager.getLogger (DelfiArticleCommentsTest.class);
 
     private WebDriver driver;
 
     @Test
     public void titleAndCommentsCountCheck() {
+        LOGGER.info ("This test is checking titles and comments count on home/article/comments pages");
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+
+        LOGGER.info ("Open browser window");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://www.delfi.lv/");
 
         //------HOME PAGE ------------
+        LOGGER.info ("Close cookie modal window");
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
 
+
         driver.findElement(ACCEPT_COOKIES_BTN).click();
 
+        LOGGER.info ("Find article number 6");
         List<WebElement> articles = driver.findElements(HOME_PAGE_ARTICLE);
         WebElement article = articles.get(5);
 
         String homePageTitle = article.findElement(HOME_PAGE_TITLE).getText();
         int homePageCommentsCount = getCommentsCount(article, HOME_PAGE_COMMENTS);
 
+        LOGGER.info ("Click home page title");
         article.findElement(HOME_PAGE_TITLE).click();
 
         //------ARTICLE PAGE ------------
