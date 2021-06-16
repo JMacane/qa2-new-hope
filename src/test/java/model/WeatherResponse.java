b56214e6d911e06756891c84fdb1cb224a9db930
Jelena Macane <jmacane@inbox.lv>
2021-06-16 20:56:14 +0300

    Gherkin addded

---
+++ src/test/java/model/WeatherResponse.java
@@ -0,0 +1,17 @@
+package model;
+
+public class WeatherResponse {
+    private Coord coord;
+    private Weather weather;
+    private String base;
+    private Main main;
+    private Integer visibility;
+    private Wind wind;
+    private Clouds clouds;
+    private Double dt;
+    private Sys sys;
+    private Integer id;
+    private String name;
+    private Integer cod;
+
+}
---
+++ src/test/java/model/Wind.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Wind {
+    private Double speed;
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public Integer getDeg() {
+        return deg;
+    }
+
+    public void setDeg(Integer deg) {
+        this.deg = deg;
+    }
+
+    private Integer deg;
+
+}
